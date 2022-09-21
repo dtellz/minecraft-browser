51e65c1fbc9231d8b9c176b8995dad107d29b809
Diego <dtellez95@gmail.com>
2022-09-21 21:11:42 +0100

    display ground in game view

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,11 +1,18 @@
 import { usePlane } from "@react-three/cannon"
+import { NearestFilter, RepeatWrapping } from "three";
 import { groundTexture } from "../images/textures";
 
 export const Ground = () => {
     const [ref] = usePlane(() => ({
-        rotation: [0, 0, 1],
+        rotation: [-Math.PI / 4, 0, 0],
         position: [0, 0, 0]
     }));
+
+    groundTexture.magFilter = NearestFilter;
+    groundTexture.wrapS = RepeatWrapping;
+    groundTexture.wrapT = RepeatWrapping;
+    groundTexture.repeat.set(100, 100);
+
     return (
         <mesh ref={ref}>
             <planeBufferGeometry attach='geometry' args={[100, 100]} />
