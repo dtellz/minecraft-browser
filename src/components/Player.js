1bf8e5788cb14fcb84ce1a357a161b25755a355c
Diego <dtellez95@gmail.com>
2022-09-22 22:11:36 +0100

    front and side movement implemented

--- src/components/Player.js
+++ src/components/Player.js
@@ -6,14 +6,15 @@ import { useKeyboard } from "../hooks/useKeyboard";
 
 
 const JUMP_FORCE = 5;
+const SPEED = 5;
 
 export const Player = () => {
-    const actions = useKeyboard();
-    console.log('actions', Object.entries(actions).filter(([k, v]) => v));
+    const { moveBackward, moveForward, moveLeft, moveRight, jump } = useKeyboard();
+    // console.log('actions', Object.entries().filter(([k, v]) => v));
 
     const { camera } = useThree();
     const [ref, api] = useSphere(() => ({
-        mass: 10,
+        mass: 1,
         type: 'Dynamic',
         position: [0, 0.5, 0],
     }));
@@ -32,7 +33,23 @@ export const Player = () => {
     useFrame(() => {
         camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
 
-        if (actions.jump && Math.abs(vel.current[1]) < 0.01) {
+        const direction = new Vector3();
+        const frontVector = new Vector3(
+            0,
+            0,
+            (moveBackward ? 1 : 0) - (moveForward ? 1 : 0),
+        );
+        const sideVector = new Vector3(
+            (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+            0,
+            0,
+        );
+
+        direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED).applyEuler(camera.rotation);
+
+        api.velocity.set(direction.x, vel.current[1], direction.z);
+
+        if (jump && Math.abs(vel.current[1]) < 0.01) {
             api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
         }
     })
